f8c957dbfb75c91dcd16da6c66bb86a807a1cb04
Kishore Vickram <23suca07@tcarts.in>
2026-01-31 09:39:55 +0530

    space added

--- src/main/java/com/college/complaintmanagement/controller/ComplaintController.java
+++ src/main/java/com/college/complaintmanagement/controller/ComplaintController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000") // React frontend
 public class ComplaintController {
 
-    private final ComplaintService service;
+     private final ComplaintService service;
 
     public ComplaintController(ComplaintService service) {
         this.service = service;
